d099b86a1131bd27940476b98b83a93681e2a58c
shuyi <osy2020@gmail.com>
2024-03-21 13:29:11 +0800

    updated filtered csv file & R code to filter csv

---
+++ csv_cleaning_retinol.R
@@ -0,0 +1,27 @@
+# Load the dplyr package
+library(dplyr)
+
+# Read the existing CSV file
+sephora_data <- read.csv("product_info.csv")
+
+# Count number of rows that have products with retinol & have a rating
+retinol_num <- sum(grepl("retinol", sephora_data$ingredients, ignore.case = TRUE) 
+                   & !is.na(sephora_data$rating))
+
+# check for number of products with retinol (for checking of csv later)
+print(retinol_num)
+
+
+# select the relevant columns required
+sephora_data <- sephora_data %>%
+  select(rating, ingredients, product_id, product_name, brand_id, brand_name)
+
+# filter out rows with products only containing retinol & contain ratings
+sephora_data <- sephora_data %>%
+  filter(!is.na(rating)&
+          grepl("retinol", ingredients, ignore.case = TRUE))
+
+# write new csv with cleaned data
+write.csv(sephora_data, "sephora_filtered.csv", row.names = FALSE)
+
+
